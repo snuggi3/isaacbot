9c7c2fe18fa98484cc3109dfc8daa7df4069c370
snuggi3 <cbtine@gmail.com>
2021-04-12 15:37:04 -0400

    Update
    Fixed iterations and writing to file

--- tutorial1.py
+++ tutorial1.py
@@ -2,26 +2,67 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-
 import time
 
-PATH = "C:\code\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+PATH = "C:\code\isaacbot\chromedriver.exe"
 
+driver = webdriver.Chrome(PATH)
 driver.get("https://platinumgod.co.uk/repentance")
+
+outfile = open('data.txt','w')
 print(driver.title)
 
-#container = driver.find_element(By.CSS_SELECTOR, 'div')
-#title = driver.find_element(By.CSS_SELECTOR, 'h2')
-#item = driver.find_element(By.CSS_SELECTOR("body > div.main > div:nth-child(3) > li:nth-child(2) > a > span")).text
+try:
+#	div = driver.find_element_by_class_name('repentanceitems-container')
+	div = driver.find_element_by_xpath('//*[@id="ht"]/body/div[2]/div[2]')
+	for item in div.find_elements_by_class_name("textbox"):
+#		print(item.get_attribute("textContent"))
+		outfile.write(item.get_attribute("textContent"))
+	
+	div = driver.find_element_by_xpath('//*[@id="ht"]/body/div[2]/div[3]')
+	for item in div.find_elements_by_class_name("textbox"):
+#		print(item.get_attribute("textContent"))
+		outfile.write(item.get_attribute("textContent"))
+	
+	div = driver.find_element_by_xpath('//*[@id="itmlist"]')
+	for item in div.find_elements_by_class_name("textbox"):
+#		print(item.get_attribute("textContent"))
+		outfile.write(item.get_attribute("textContent"))
+	
+	div = driver.find_element_by_xpath('//*[@id="ht"]/body/div[2]/div[5]')
+	for item in div.find_elements_by_class_name("textbox"):
+#		print(item.get_attribute("textContent"))
+		outfile.write(item.get_attribute("textContent"))
 
-items_list = []
+	div = driver.find_element_by_xpath('//*[@id="ht"]/body/div[2]/div[6]')
+	for item in div.find_elements_by_class_name("textbox"):
+#		print(item.get_attribute("textContent"))
+		outfile.write(item.get_attribute("textContent"))
+	
+	div = driver.find_element_by_xpath('//*[@id="ht"]/body/div[2]/div[7]')
+	for item in div.find_elements_by_class_name("textbox"):
+#		print(item.get_attribute("textContent"))
+		outfile.write(item.get_attribute("textContent"))
+	
+	div = driver.find_element_by_xpath('//*[@id="ht"]/body/div[2]/div[8]')
+	for item in div.find_elements_by_class_name("textbox"):
+#		print(item.get_attribute("textContent"))
+		outfile.write(item.get_attribute("textContent"))
 
-main = driver.find_element_by_css_selector(".p").text
-#//*[@id="ht"]/body/div[2]/div[2]/li[1]/a/span
+	div = driver.find_element_by_xpath('//*[@id="ht"]/body/div[2]/div[9]')
+	for item in div.find_elements_by_class_name("textbox"):
+#		print(item.get_attribute("textContent"))
+		outfile.write(item.get_attribute("textContent"))
+	
+	div = driver.find_element_by_xpath('//*[@id="ht"]/body/div[2]/div[10]')
+	for item in div.find_elements_by_class_name("textbox"):
+#	PRINTING TO CONSOLE SINCE IT WON'T WRITE TO FILE	
+		print(item.get_attribute("textContent"))
 
+except Exception as e:
+    print(f"Failed for '{e}'")
 
-print(main)
+finally:
+    driver.quit()
 
-driver.close()
 print('Done')
